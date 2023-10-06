de46a5a7d2dec97bbddcc5fb1c8d1c700b50849c
hazem-abdelghany20 <hazemabdelghany43@gmail.com>
2023-10-06 10:17:37 +0300

    Added get one patient and get one pharmacist, with restriction to be an admin

--- backend/controllers/pharmacistController.js
+++ backend/controllers/pharmacistController.js
@@ -1,6 +1,7 @@
-const Medicine = require('../models/medicineModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const factory = require('./handlerFactory');
+const Medicine = require("../models/medicineModel");
+const Pharmacist = require("../models/pharmacistModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const factory = require("./handlerFactory");
 
-
+exports.getPharmacist = factory.getOne(Pharmacist);
--- backend/routes/pharmacistRoutes.js
+++ backend/routes/pharmacistRoutes.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pharmacistController = require('../controllers/pharmacistController');
-const authController = require('../controllers/authController');
+const pharmacistController = require("../controllers/pharmacistController");
+const authController = require("../controllers/authController");
 
-
+router
+  .route("/getpharmacist/:id")
+  .get(
+    authController.protect,
+    authController.restrictTo("administrator"),
+    pharmacistController.getPharmacist
+  );
 
 module.exports = router;
