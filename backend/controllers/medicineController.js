caf8b1e0afdb0baac3754793a0e9bd9a62cf4ec0
Saraahmedd <saraahmedsaber@gmail.com>
2023-10-07 23:45:40 +0300

    restrictions on update -- done but needs debugging

--- backend/controllers/medicineController.js
+++ backend/controllers/medicineController.js
@@ -7,6 +7,32 @@ exports.getAllMedicinesForUserAndAdmin = factory.getAll(Medicine,'-sales -quanti
 exports.getAllMedicinesForPharmacist = factory.getAll(Medicine);
 
 exports.getMedicineById = factory.getOne(Medicine);
+
+exports.updateMedicine = Medicine =>
+    catchAsync(async (req, res, next) => {
+        req.body.name = undefined;
+        req.body.description = undefined;
+        req.body.medicinalUses = undefined;
+        req.body.medicineIngredients = undefined;
+        factory.updateOne(Medicine)(req, res, next);
+    });
+
+// exports.updateMedicine = Medicine =>
+//     catchAsync(async (req, res, next) => {
+//         const allowedFields = ['price', 'quantity', 'sales']; 
+//         const updates = {};
+
+//         allowedFields.forEach(field => {
+//             if (req.body[field] !== undefined) {
+//                 updates[field] = req.body[field];
+//             }
+//         });
+
+//         // Merge the filtered updates with the existing req.body
+//         req.body = { ...req.body, ...updates };
+        
+//         factory.updateOne(Medicine)(req, res, next);
+//     });
+
 exports.createNewMedicine = factory.createOne(Medicine)
-exports.updateMedicine = factory.updateOne(Medicine)
 exports.deleteMedicine = factory.deleteOne(Medicine);
