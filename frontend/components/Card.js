f44fd02d82d6c1f219a930182d452975eb35bcf1
Saraahmedd <saraahmedsaber@gmail.com>
2023-11-07 23:50:06 +0200

    admin - pharmacists/applications screens

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,18 +1,20 @@
 const { Button } = require("./Button");
 
-function Card ({ title, subtitle, text, className, image = "", buttonText = "", headerText = "", onClick, onClickButton, children }) {
+function Card ({ title, subtitle, text, className, image = "", buttonText = "", headerText = "", onClick, onClickButton, children, buttonClass= "" }) {
 
     return (
         <>
-            <div className={`card ${onClick ? 'hover-button' : ''} ${className}`} role={onClick ? "button" : ""}>
+            <div className={`card global-text shadow border-0 ${onClick ? 'hover-button' : ''} ${className}`} role={onClick ? "button" : ""}>
+                <div className="bg-primary px-2 rounded"></div>
                 {image}
                 {headerText && <div className="card-header">{headerText}</div>}
                 <div className="card-body">
-                    {title && <h5 className="card-title">{title}</h5>}
+                    {title && <h5 className="text-primary" style={{ fontWeight: 'bold' , fontSize: '30px'}}>{title}</h5>}
+                    <hr />
                     {subtitle && <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>}
-                    {text && <p className="card-text">{text}</p>}
+                    {text && <h8 className="card-text">{text}</h8>}
                     {children}
-                    {buttonText && <Button onClick={onClickButton} text={buttonText} variant="md" />}
+                    {buttonText && <Button onClick={onClickButton} text={buttonText} variant="md" className={`mx-auto ${buttonClass}`} />}
                 </div>
             </div>
         </>
