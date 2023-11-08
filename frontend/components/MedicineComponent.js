1b0145a5fc2de9c02302595047624740c4ee0a61
Hazem Abdelghany <hazemabdelghany43@gmail.com>
2023-11-08 17:16:56 +0200

    update

--- frontend/components/MedicineComponent.js
+++ frontend/components/MedicineComponent.js
@@ -5,10 +5,14 @@ import { Card } from "./Card";
 import DescriptionModal from "./MedicineDescriptionModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch, useSelector } from "react-redux";
+import EditableField from "./EditableField";
+import { Button } from "./Button";
+import AddModal from "./AddModal";
 import { getMedicinesAction } from "@/app/redux/actions/medicineActions";
 import { login } from "@/app/redux/actions/authActions";
 
 function MedicineComponent({ title }) {
+  const [modalAddShow, setModalAddShow] = useState(false);
   const [selectedMedicinalUse, setSelectedMedicinalUse] = useState(null);
   const [modalDescShow, setModalDescShow] = useState(false);
   const [selectedMedicine, setSelectedMedicine] = useState(null);
@@ -73,12 +77,13 @@ function MedicineComponent({ title }) {
           </select>
         </div>
       </div>
-                       <Button text="Add Product" className="add-button m-3 mb-4"   onClick={() => setModalAddShow(true)}/>
-    
-           <AddModal
-            show={modalAddShow}
-            onHide={() => setModalAddShow(false)} 
-          />
+      <Button
+        text="Add Product"
+        className="add-button m-3 mb-4"
+        onClick={() => setModalAddShow(true)}
+      />
+
+      <AddModal show={modalAddShow} onHide={() => setModalAddShow(false)} />
       <div className="container-fluid ">
         <div className="row mx-4">
           {medicines?.map((medicine) => (
@@ -116,91 +121,104 @@ function MedicineComponent({ title }) {
             />
           ))}
 
-{selectedMedicine && (
-        <DescriptionModal
-          show={modalDescShow}
-          onHide={() => setModalDescShow(false)}
-          // onEdit={handleEdit}
-          header={selectedMedicine.name}
-          subheader={""}
-          text={
-            <div>
-            <div className="info-container d-flex align-items">
-              <h5>Price</h5>
-            
-            </div>
-            <EditableField
-            
-              value={selectedMedicine.price}
-              id={selectedMedicine._id}
-              nameOfField={"price"}
-              hide={() => setModalDescShow(false)}
-              edit={true}
-              onSave={(newValue) => handleSave(newValue, 'price')}
-            />
-          
-            <div className="info-container d-flex align-items">
-              <h5>Quantity</h5>
-            
-            </div>
-            <EditableField
-               id={selectedMedicine._id}
-              value={selectedMedicine.quantity}
-              nameOfField={"quantity"}
-              hide={() => setModalDescShow(false)}
-              edit={true}
-              onSave={(newValue) => handleSave(newValue, 'quantity')}
-            />
-          
-            <div className="info-container d-flex align-items">
-              <h5>Description</h5>
-            
-            </div>
-            <EditableField
-             id={selectedMedicine._id}
-             hide={() => setModalDescShow(false)}
-             nameOfField={"description"}
-             edit={false}
-             
-              value={selectedMedicine.description}
-              onSave={(newValue) => handleSave(newValue, 'description')}
-            />
-          
-            <div className="info-container d-flex align-items">
-              <h5>Medicinal Uses</h5>
-             
-            </div>
-            <EditableField
-              id={selectedMedicine._id}
-              hide={() => setModalDescShow(false)}
-              value={selectedMedicine.medicinalUses.join(', ')}
-              nameOfField={"medicinalUses"}
-              edit={false}
-              onSave={(newValue) => handleSave(newValue.split(',').map(item => item.trim()), 'medicinalUses')}
-            />
-          
-            <div className="info-container d-flex align-items">
-              <h5>Medicine Ingredients</h5>
-             
-            </div>
-            <EditableField
-             id={selectedMedicine._id}
-             nameOfField={"medicineIngredients"}
-             hide={() => setModalDescShow(false)}
-             edit={true}
-              
-              value={selectedMedicine.medicineIngredients.join(', ')}
-              onSave={(newValue) => handleSave(newValue.split(',').map(item => item.trim()), 'medicineIngredients')}
-            />
-          </div>
-          
-          }
-          
-          image={<img src={selectedMedicine?.imageURL? selectedMedicine.imageURL : '/medication.svg'}  alt="Image"  style={{  float: 'right',  marginLeft: '10px',  maxHeight: '200px', maxWidth: '150px',}} />}
-        />
+          {selectedMedicine && (
+            <DescriptionModal
+              show={modalDescShow}
+              onHide={() => setModalDescShow(false)}
+              // onEdit={handleEdit}
+              header={selectedMedicine.name}
+              subheader={""}
+              text={
+                <div>
+                  <div className="info-container d-flex align-items">
+                    <h5>Price</h5>
+                  </div>
+                  <EditableField
+                    value={selectedMedicine.price}
+                    id={selectedMedicine._id}
+                    nameOfField={"price"}
+                    hide={() => setModalDescShow(false)}
+                    edit={true}
+                    onSave={(newValue) => handleSave(newValue, "price")}
+                  />
 
-        
-      )}
+                  <div className="info-container d-flex align-items">
+                    <h5>Quantity</h5>
+                  </div>
+                  <EditableField
+                    id={selectedMedicine._id}
+                    value={selectedMedicine.quantity}
+                    nameOfField={"quantity"}
+                    hide={() => setModalDescShow(false)}
+                    edit={true}
+                    onSave={(newValue) => handleSave(newValue, "quantity")}
+                  />
+
+                  <div className="info-container d-flex align-items">
+                    <h5>Description</h5>
+                  </div>
+                  <EditableField
+                    id={selectedMedicine._id}
+                    hide={() => setModalDescShow(false)}
+                    nameOfField={"description"}
+                    edit={false}
+                    value={selectedMedicine.description}
+                    onSave={(newValue) => handleSave(newValue, "description")}
+                  />
+
+                  <div className="info-container d-flex align-items">
+                    <h5>Medicinal Uses</h5>
+                  </div>
+                  <EditableField
+                    id={selectedMedicine._id}
+                    hide={() => setModalDescShow(false)}
+                    value={selectedMedicine.medicinalUses.join(", ")}
+                    nameOfField={"medicinalUses"}
+                    edit={false}
+                    onSave={(newValue) =>
+                      handleSave(
+                        newValue.split(",").map((item) => item.trim()),
+                        "medicinalUses"
+                      )
+                    }
+                  />
+
+                  <div className="info-container d-flex align-items">
+                    <h5>Medicine Ingredients</h5>
+                  </div>
+                  <EditableField
+                    id={selectedMedicine._id}
+                    nameOfField={"medicineIngredients"}
+                    hide={() => setModalDescShow(false)}
+                    edit={true}
+                    value={selectedMedicine.medicineIngredients.join(", ")}
+                    onSave={(newValue) =>
+                      handleSave(
+                        newValue.split(",").map((item) => item.trim()),
+                        "medicineIngredients"
+                      )
+                    }
+                  />
+                </div>
+              }
+              image={
+                <img
+                  src={
+                    selectedMedicine?.imageURL
+                      ? selectedMedicine.imageURL
+                      : "/medication.svg"
+                  }
+                  alt="Image"
+                  style={{
+                    float: "right",
+                    marginLeft: "10px",
+                    maxHeight: "200px",
+                    maxWidth: "150px",
+                  }}
+                />
+              }
+            />
+          )}
         </div>
       </div>
       {/* <Footer/> */}
