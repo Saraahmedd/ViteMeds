809f378c73077daafe4566888d6dbcbf2a85f5c7
omartamer-git <omartamer02@hotmail.com>
2023-12-07 01:45:02 +0200

    Init

--- newfrontend/src/app/dashboard/layout.js
+++ newfrontend/src/app/dashboard/layout.js
@@ -2,27 +2,27 @@ import Sidebar from "@/components/Sidebar";
 import { cookies } from "next/dist/client/components/headers";
 import { redirect } from "next/navigation";
 
-const checkLoggedIn = async () => {
-    const cookiesString = cookies().getAll()
-        .map((cookie) => `${cookie.name}=${encodeURIComponent(cookie.value)}`)
-        .join('; ');
+// const checkLoggedIn = async () => {
+//     const cookiesString = cookies().getAll()
+//         .map((cookie) => `${cookie.name}=${encodeURIComponent(cookie.value)}`)
+//         .join('; ');
 
-    const isLoggedIn = await fetch("http://3.73.222.159:3000/staff/session", {
-        method: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Cookie': cookiesString
-        },
-    });
+//     const isLoggedIn = await fetch("http://3.73.222.159:3000/staff/session", {
+//         method: "GET",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Cookie': cookiesString
+//         },
+//     });
 
-    // if(isLoggedIn.status >= 400) {
-    //     redirect('/');
-    //     return;
-    // }
-};
+//     // if(isLoggedIn.status >= 400) {
+//     //     redirect('/');
+//     //     return;
+//     // }
+// };
 
 export default async function DashboardLayout({ children }) {
-    await checkLoggedIn();
+    // await checkLoggedIn();
 
     return (
         <>
