c2fb69adf1eee7f2f13eb54099a8b2e277210fe3
omartamer-git <omartamer02@hotmail.com>
2023-12-07 03:51:26 +0200

    Product Page

--- newfrontend/src/app/dashboard/products/page.js
+++ newfrontend/src/app/dashboard/products/page.js
@@ -5,15 +5,14 @@ export default function Dashboard() {
     return (
         <>
             <Card className="grow flex-1">
-                <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-2">
+                <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-4">
                     {
                         (Array.from({ length: 6 })).map((item, index) => (
-                            <ProductCard />
+                            <ProductCard key={index} id={index} name="Medicine Example" image={`/med${(index % 3) + 1}.png`} price={200} initialQuantity={0} />
                         ))
                     }
                 </Grid>
             </Card>
-            <ProductCard />
         </>
     );
 }
