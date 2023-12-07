203df12a61e8c646ae42f280037ff22894a8c152
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-08 01:02:39 +0200

    Turn off protection

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -190,7 +190,7 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser;
-  res.locals?.user = currentUser;
+  res.locals.user = currentUser;
   next();
 });
 
