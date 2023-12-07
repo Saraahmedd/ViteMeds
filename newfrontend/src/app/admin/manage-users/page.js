4aa5be6579f1230173d0c09f0bf17e69ebf728c6
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-08 00:39:41 +0200

    Solve conflicts

--- newfrontend/src/app/admin/manage-users/page.js
+++ newfrontend/src/app/admin/manage-users/page.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react'
 
-export const page = () => {
-  return <div>page</div>;
-};
+const Page = () => {
+  return (
+    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+        <li class="me-2">
+            <a href="#" aria-current="page" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Admins</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Pharmacists</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Patients</a>
+        </li>
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Applications</a>
+        </li>
+</ul>
+
+  )
+}
+
+export default Page
