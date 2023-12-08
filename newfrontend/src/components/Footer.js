e1a091b6f69be642a8c7b75fee7eee61fefada06
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-08 19:22:57 +0200

    Footer

--- newfrontend/src/components/Footer.js
+++ newfrontend/src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Footer = () => {
   return (
-<<<<<<< HEAD
     <div className="mx-3 px-[32px] sm:ml-64 ">
       <footer className="bg-white rounded-lg shadow dark:bg-gray-900 my-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -47,21 +46,6 @@ const Footer = () => {
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
             <a href="https://flowbite.com/" className="hover:underline">
-=======
-    <footer className="rounded-lg shadow dark:bg-gray-900 my-4">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
->>>>>>> dce52d4843ccd0713dbca631dcc550e87cd6f5c8
               Pharmacy
             </a>
             . All Rights Reserved.
