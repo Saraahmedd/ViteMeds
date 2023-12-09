d98d2980a12382d973e7278e098b4a64f4ca5323
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-10 00:21:06 +0200

    Add medicine

--- newfrontend/src/app/patient/products/page.js
+++ newfrontend/src/app/patient/products/page.js
@@ -2,17 +2,84 @@
 
 import { addToCart, viewCart } from "@/app/redux/actions/cartActions";
 import {
+  addMedicine,
   editMedicine,
   getMedicinesAction,
 } from "@/app/redux/actions/medicineActions";
+import { BottomCallout } from "@/components/BottomCallout";
+import { FileUpload } from "@/components/FileUpload";
+import { Modal } from "@/components/Modal";
 import { ProductCard } from "@/components/ProductCard";
-import { Card, Grid, Select, SelectItem, TextInput } from "@tremor/react";
+import {
+  Button,
+  Card,
+  Grid,
+  Select,
+  SelectItem,
+  TextInput,
+} from "@tremor/react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Products() {
   const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    name: null,
+    quantity: "",
+    medicinalUses: [],
+    description: "",
+    image: null,
+    price: "",
+    medicineIngredients: [],
+  });
+  const [visible, setVisible] = useState(false);
 
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleInputChange = (name, value) => {
+    console.log(value);
+    if (name === "medicineIngredients" || name === "medicinalUses") {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value.split(",").map((ingredient) => ingredient.trim()),
+      }));
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleFileUpload = (file) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      image: file,
+    }));
+  };
+
+  const handleAddMedicine = () => {
+    const combinedFormData = new FormData();
+    for (const key in formData) {
+      if (formData.hasOwnProperty(key)) {
+        // Check if the key is an array (medicinalUses and medicineIngredients)
+        if (Array.isArray(formData[key])) {
+          formData[key].forEach((item, index) => {
+            combinedFormData.append(`${key}[${index}]`, item);
+          });
+        } else {
+          combinedFormData.append(key, formData[key]);
+        }
+      }
+    }
+    dispatch(addMedicine(combinedFormData));
+  };
+
+  const {
+    success: addSuccess,
+    error: addError,
+    loading: addLoading,
+  } = useSelector((state) => state.addMedicineReducer);
   const medicines = useSelector(
     (state) => state.getMedicinesReducer.medicines?.data
   );
@@ -29,7 +96,7 @@ export default function Products() {
   const cartLoading = useSelector((state) => state.addToCartReducer.loading);
   useEffect(() => {
     dispatch(getMedicinesAction({ ...name, ...medUse }));
-  }, [dispatch, name, medUse, cartLoading]);
+  }, [dispatch, name, medUse, cartLoading, addLoading]);
   useEffect(() => {
     dispatch(viewCart());
     console.log("cart changed");
@@ -53,17 +120,157 @@ export default function Products() {
       return 0;
     }
   }
+  useEffect(() => {
+    if (addSuccess) {
+      setFormData({});
+      setModalVisible(false);
+    }
+  }, [addLoading]);
 
   return (
     <Card className="grow flex-1">
+      {addSuccess && (
+        // Show success message for user removal
+        <BottomCallout
+          message="Medicine added successfully"
+          variant="success"
+          visible={true}
+          setVisible={setVisible}
+        />
+      )}
+
+      {addError && (
+        // Show error message for registration failure
+        <BottomCallout
+          message={addError || "Medicine already exists with that name"}
+          variant="error"
+          visible={true}
+          setVisible={setVisible}
+        />
+      )}
       <h1 className="font-bold text-2xl">Products</h1>
       <br></br>
-      <TextInput
-        onChange={(e) => setName({ name: { regex: e.target.value } })}
-        type="search"
-        className="w-60"
-        placeholder="Search For Medicine"
-      ></TextInput>
+      <div className="flex justify-between">
+        <TextInput
+          onChange={(e) => setName({ name: { regex: e.target.value } })}
+          type="search"
+          className="w-60"
+          placeholder="Search For Medicine"
+        />
+        <Button
+          variant="secondary"
+          className=" px-4 py-2 rounded"
+          onClick={() => setModalVisible(true)}
+        >
+          New Medicine
+        </Button>
+      </div>
+      <Modal visible={modalVisible} setVisible={setModalVisible}>
+        <div className="p-4 flex flex-col">
+          <h1 className="text-center">New Medicine</h1>
+          <br />
+          <TextInput
+            placeholder="Name"
+            className="w-full my-4 px-8 py-4 rounded-lg font-medium   placeholder-gray-500 text-lg  "
+            value={formData.name}
+            error={
+              addError && formData.name === null && "Please fill in this field"
+            }
+            onChange={(e) => handleInputChange("name", e.target.value)}
+            required
+            errorMesssage={
+              addError && formData.name === null && "Please fill in this field"
+            }
+          />
+          <TextInput
+            placeholder="Description"
+            className="w-full my-4 px-8 py-4 rounded-lg font-medium   placeholder-gray-500 text-lg  "
+            value={formData.description}
+            onChange={(e) => handleInputChange("description", e.target.value)}
+            errorMesssage={
+              addError &&
+              formData.description == "" &&
+              "Please fill in this field"
+            }
+          />
+          <TextInput
+            placeholder="Quantity"
+            className="w-full my-4 px-8 py-4 rounded-lg font-medium   placeholder-gray-500 text-lg  "
+            type="number"
+            value={formData.quantity}
+            onChange={(e) => handleInputChange("quantity", e.target.value)}
+            error={
+              addError &&
+              formData.quantity === "" &&
+              "Please fill in this field"
+            }
+            errorMessage={
+              addError &&
+              formData.quantity === "" &&
+              "Please fill in this field"
+            }
+          />
+
+          <TextInput
+            placeholder="Medicine uses (Enter them comma separated)"
+            className="w-full my-4 px-8 py-4 rounded-lg font-medium placeholder-gray-500 text-lg"
+            value={formData.medicinalUses?.join(",")}
+            onChange={(e) => handleInputChange("medicinalUses", e.target.value)}
+            required
+            error={addError && formData.medicinalUses == []}
+            errorMessage={
+              addError &&
+              formData.medicinalUses == [] &&
+              "Please fill in this field"
+            }
+          />
+          <TextInput
+            placeholder="Price"
+            className="w-full my-4  px-8 py-4 rounded-lg font-medium   placeholder-gray-500 text-lg  "
+            type="number"
+            value={formData.price}
+            onChange={(e) => handleInputChange("price", e.target.value)}
+            required
+            errorMessage={
+              addError && formData.price === "" && "Please fill in this field"
+            }
+            error={
+              addError && formData.price === "" && "Please fill in this field"
+            }
+          />
+          <TextInput
+            className="w-full my-4 px-8 py-4 rounded-lg font-medium placeholder-gray-500 text-lg"
+            placeholder="Medicine Ingredients (Enter them comma separated)"
+            value={formData.medicineIngredients?.join(",")}
+            onChange={(e) =>
+              handleInputChange("medicineIngredients", e.target.value)
+            }
+            errorMessage={
+              addError &&
+              formData.medicineIngredients == [] &&
+              "Please fill in this field"
+            }
+            error={
+              addError &&
+              formData.medicineIngredients == [] &&
+              "Please fill in this field"
+            }
+            required
+          />
+
+          <FileUpload
+            className="w-full my-4  px-8 py-4"
+            placeholder="Image"
+            callBackFiles={handleFileUpload}
+            required
+          />
+          <br></br>
+
+          <Button className="self-end" onClick={handleAddMedicine}>
+            Add Medicine
+          </Button>
+        </div>
+      </Modal>
       <br></br>
       <select
         onChange={(e) =>
