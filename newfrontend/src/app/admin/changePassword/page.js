1b2ab6a25d21f820aba03766be381189b8e38943
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-10 05:31:09 +0200

    password

---
+++ newfrontend/src/app/admin/changePassword/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChangePassword from "@/components/ChangePassword";
+
+const page = () => {
+  return (
+    <div className="justify-center m-auto my-auto">
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default page;
