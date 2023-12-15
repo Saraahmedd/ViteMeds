9c130e0da077b73721545116e4efbc34a8d404c4
omartamer-git <omartamer02@hotmail.com>
2023-12-15 05:52:23 +0200

    Autocomplete off

--- newfrontend/src/app/layout.js
+++ newfrontend/src/app/layout.js
@@ -1,12 +1,7 @@
+"use client"
 import Header from "@/components/Header";
 import "./globals.css";
 import localFont from "next/font/local";
-
-export const metadata = {
-  title: "Pharmacy",
-  description: "Pharmacy",
-};
-
 import { ReduxProvider } from "./redux/provider";
 import Footer from "@/components/Footer";
 
@@ -35,14 +30,24 @@ const myFont = localFont({
   ],
 });
 
+const role = JSON.parse(localStorage.getItem("userInfo")).data.user.role;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={myFont.className + " min-h-screen flex flex-col "}>
-        <ReduxProvider>{children} </ReduxProvider>
-        {/* <div className="flex-1 grow"></div> */}
-        <Footer />
+      <body className={myFont.className + " min-h-screen flex flex-col"}>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+        <Footer role={role} />
       </body>
+      <script src="/jquery-3.7.1.min.js" type="text/javascript"></script>
+      <script>
+        $(document).ready(function(){
+          $('input').attr('autocomplete','off');
+          $('form').attr('autocomplete','off');
+        });
+      </script>
     </html>
   );
 }
