8764206248a53a8f9ef6ba098e18ef9ed87f233c
abdullah hatem <abdullah.hatem@lastingdynamics.com>
2023-12-17 01:35:09 +0200

    Required docs

--- newfrontend/src/app/signup/pharmacist/page.js
+++ newfrontend/src/app/signup/pharmacist/page.js
@@ -445,6 +445,11 @@ const SignupPharmacist = () => {
 
               <Col>
                 <Button
+                  disabled={
+                    files.document1 === null ||
+                    files.document2 === null ||
+                    files.document3 === null
+                  }
                   variant="primary"
                   className="h-full tracking-wide font-semibold text-gray-100 w-full py-4 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                   onClick={handleSignUp}
